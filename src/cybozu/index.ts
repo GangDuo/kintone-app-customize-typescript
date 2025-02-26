ed07098fcbf9f7b2eb00f41274f2aff73fb68ff9
GangDuo <5295036+GangDuo@users.noreply.github.com>
2025-02-26 12:48:48 +0900

    Cybozu.Organizationを値オブジェクト化する。

--- src/cybozu/index.ts
+++ src/cybozu/index.ts
@@ -14,9 +14,23 @@ export namespace Cybozu {
     }
 
     export class Organization {
-        static async getOrganizations(organizationCodes: string[]): Promise<CybozuTypes.Organization[]> {
+        private organization: CybozuTypes.Organization;
+
+        private constructor(organization: CybozuTypes.Organization) {
+            this.organization = organization;
+        }
+
+        get id(): number { return this.organization.id; }
+        get code(): string { return this.organization.code; }
+        get name(): string { return this.organization.name; }
+        get localName(): string { return this.organization.localName; }
+        get localNameLocale(): string { return this.organization.localNameLocale; }
+        get parentCode(): string | null { return this.organization.parentCode; }
+        get description(): string { return this.organization.description; }
+
+        static async getOrganizations(organizationCodes: string[]): Promise<Organization[]> {
             const { organizations } = await kintone.api(kintone.api.url("/v1/organizations.json"), 'GET', { codes: organizationCodes });
-            return organizations;
+            return organizations.map((val: CybozuTypes.Organization) => new Organization(val));
         }
 
         getUsers() { }
