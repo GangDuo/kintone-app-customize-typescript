a3ff1e0d9eb5bb2112ec3e2929a683c119e0d62a
GangDuo <5295036+GangDuo@users.noreply.github.com>
2025-02-27 16:57:40 +0900

    ユーザが所属している組織を遡って取得する。

--- src/cybozu/index.ts
+++ src/cybozu/index.ts
@@ -1,6 +1,50 @@
 import type { CybozuTypes } from "./types";
 
 export namespace Cybozu {
+    export type CodeNamePair = {
+        code:string;
+        name:string;
+    };
+
+    export class Client {
+        static async goUpstream(
+            userCode: string | null,
+            organizationCode: string | null = null,
+            footprints: CodeNamePair[] = []
+        ): Promise<CodeNamePair[]> {
+            let parentCode = null;
+            const signed = [...footprints];
+
+            if(userCode !== null) {
+                const organizationTitles = await Cybozu.User.getOrganizations(userCode);
+                if (organizationTitles.length === 0 || organizationTitles[0].organization.parentCode === null) {
+                    return footprints;
+                }
+                signed.push({
+                    code: organizationTitles[0].organization.code,
+                    name: organizationTitles[0].organization.name
+                });
+                parentCode = organizationTitles[0].organization.parentCode;
+            }
+
+            if(organizationCode !== null) {
+                const organizations = await Cybozu.Organization.getOrganizations([organizationCode]);
+                if (organizations.length === 0) {
+                    return footprints;
+                }
+
+                signed.push({
+                    code: organizations[0].code,
+                    name: organizations[0].name
+                });
+                parentCode = organizations[0].parentCode;
+            }
+
+            if(parentCode === null) return signed;
+            return Client.goUpstream(null, parentCode, signed);
+        }
+    }
+
     export class User {
         getUsers() { }
 
