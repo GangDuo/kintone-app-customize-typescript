d0834c409f5f4377f222650303129c00b52b1ed6
GangDuo <5295036+GangDuo@users.noreply.github.com>
2025-10-24 19:01:15 +0900

    feat(build): コード難読化を含む本番ビルドの追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,50 @@
 const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const WebpackObfuscator = require('webpack-obfuscator');
 
-module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  entry: {
-    'sample': './src/sample.ts',
-    'crc32': './src/crc32.ts',
-    'trace-back-to-grandparents': './src/trace-back-to-grandparents.ts'
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
 
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  const config = {
+    devtool: isProduction ? false : 'inline-source-map',
+    entry: {
+      'sample': './src/sample.ts',
+      'crc32': './src/crc32.ts',
+      'trace-back-to-grandparents': './src/trace-back-to-grandparents.ts'
+    },
 
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json'],
-    fallback: {
-      buffer: require.resolve('buffer')
-    }
-  },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
 
-  module: {
-    rules: [{test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/}],
-  },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+      fallback: {
+        buffer: require.resolve('buffer')
+      }
+    },
 
-  plugins: [
-    new ForkTsCheckerWebpackPlugin(),
-  ]
+    module: {
+      rules: [{test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/}],
+    },
+
+    plugins: [
+      new ForkTsCheckerWebpackPlugin(),
+    ]
+  };
+
+  if (isProduction) {
+    config.plugins.push(new WebpackObfuscator({
+      rotateStringArray: true,
+      stringArray: true,
+      stringArrayThreshold: 0.75,
+      debugProtection: true,
+      selfDefending: true,
+      splitStrings: true,
+      splitStringsChunkLength: 5,
+    }));
+  }
+
+  return config;
 };
